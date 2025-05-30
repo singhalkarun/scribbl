c0c8c0546debb5369921068e11c52ba24c90cd32
Karun Agarwal <singhalkarun@gmail.com>
2025-05-31 01:44:42 +0530

    remove db related unused code from test/ also

--- scribbl_backend/test/support/conn_case.ex
+++ scribbl_backend/test/support/conn_case.ex
@@ -6,13 +6,6 @@ defmodule ScribblBackendWeb.ConnCase do
   Such tests rely on `Phoenix.ConnTest` and also
   import other functionality to make it easier
   to build common data structures and query the data layer.
-
-  Finally, if the test case interacts with the database,
-  we enable the SQL sandbox, so changes done to the database
-  are reverted at the end of every test. If you are using
-  PostgreSQL, you can even run database tests asynchronously
-  by setting `use ScribblBackendWeb.ConnCase, async: true`, although
-  this option is not recommended for other databases.
   """
 
   use ExUnit.CaseTemplate
@@ -31,8 +24,7 @@ defmodule ScribblBackendWeb.ConnCase do
     end
   end
 
-  setup tags do
-    ScribblBackend.DataCase.setup_sandbox(tags)
+  setup _tags do
     {:ok, conn: Phoenix.ConnTest.build_conn()}
   end
 end
--- scribbl_backend/test/test_helper.exs
+++ scribbl_backend/test/test_helper.exs
@@ -1,2 +1,1 @@
 ExUnit.start()
-Ecto.Adapters.SQL.Sandbox.mode(ScribblBackend.Repo, :manual)
