1c532d6401aa00308f89947b8a5b915eeca5e716
Prateek Jakhar <prtkjakhar@gmail.com>
2025-06-06 15:31:20 +0530

    feat: added join/create room functionality (#42)

--- scribbl_frontend/src/lib/socket.ts
+++ scribbl_frontend/src/lib/socket.ts
@@ -4,10 +4,13 @@ import { Socket } from "phoenix";
 
 export const createSocket = (userId?: string) => {
   console.log("[Socket] Creating socket with userId:", userId);
-  const socket = new Socket(process.env.NEXT_PUBLIC_SOCKET_URL || "", {
-    params: {
-      user_id: userId || "",
-    },
-  });
+  const socket = new Socket(
+    process.env.NEXT_PUBLIC_BACKEND_URL + "/socket" || "",
+    {
+      params: {
+        user_id: userId || "",
+      },
+    }
+  );
   return socket;
 };
