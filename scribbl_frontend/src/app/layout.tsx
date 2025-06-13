e6c94e0dcf1c0383c3736d20b40ce15d78409a2d
Karun Agarwal <singhalkarun@gmail.com>
2025-06-14 01:44:08 +0530

    integrate hotjar

--- scribbl_frontend/src/app/layout.tsx
+++ scribbl_frontend/src/app/layout.tsx
@@ -174,6 +174,20 @@ export default function RootLayout({
                 gtag('config', 'G-1YN078DXYJ');
               `}
             </Script>
+            
+            {/* Hotjar Tracking Code */}
+            <Script id="hotjar-tracking" strategy="afterInteractive">
+              {`
+                (function(h,o,t,j,a,r){
+                    h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                    h._hjSettings={hjid:6434778,hjsv:6};
+                    a=o.getElementsByTagName('head')[0];
+                    r=o.createElement('script');r.async=1;
+                    r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                    a.appendChild(r);
+                })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+              `}
+            </Script>
           </>
         )}
       </head>
